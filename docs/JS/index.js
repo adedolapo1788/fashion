a56fe6a050de112c7815db2ad6f329a9058c5f5f
Olamide Adedolapo <olamidetunechi@gmail.com>
2018-12-11 18:41:28 +0100

    string validation

--- docs/JS/index.js
+++ docs/JS/index.js
@@ -35,3 +35,27 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+// arrray for accepting inputs
+
+var items = [];
+
+function guardarNumeros() {
+  boxvalue = document.getElementById('fname').value;
+  boxvalue2 = document.getElementById('lname').value;
+  boxvalue3 = document.getElementById('text-content').value;
+  items.push(boxvalue);
+  items.push(boxvalue2);
+  items.push(boxvalue3);
+  console.log(items);
+  return false; // stop submission
+}
+
+//  turning array elements into string for displaying
+function mySubmit() {
+    items.toString();
+    document.getElementById("demo").innerHTML = items;
+
+}
+
